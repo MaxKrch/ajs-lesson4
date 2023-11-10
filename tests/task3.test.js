6e94e745fe3cb82e94d253d616161b0d8acc943e
Max Kyrchanov <max.kyrchanov@gmail.com>
2023-11-10 22:47:16 +0500

    complete task3

---
+++ tests/task3.test.js
@@ -0,0 +1,30 @@
+import fetchData from "../src/js/http.js";
+import { getLevel } from "../src/js/task3.js";
+
+
+jest.mock('../src/js/http.js');
+
+beforeEach(() => {
+	jest.resetAllMocks();
+});
+
+test('Request with correct id', () => {
+	fetchData.mockReturnValue({status: 'ok',
+		level: 5
+})
+
+	const received = getLevel(1);
+	const expected = "Ваш текущий уровень: 5"
+	expect(fetchData).toBeCalledWith('https://server/user/1');
+	expect(received).toBe(expected);
+})
+
+test('Request with uncorrect id', () => {
+	fetchData.mockReturnValue({status: 'no'
+})
+
+	const received = getLevel('chek');
+	const expected = "Информация об уровне временно недоступна"
+	expect(fetchData).toBeCalledWith('https://server/user/chek');
+	expect(received).toBe(expected);
+})
