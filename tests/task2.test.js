1ec0b4ee58ccb5da00a1591b6e332e46e7fc8fff
Max Kyrchanov <max.kyrchanov@gmail.com>
2023-11-10 21:24:13 +0500

    fix spaces

--- tests/task2.test.js
+++ tests/task2.test.js
@@ -10,15 +10,15 @@ test('enpty list characters', () => {
 
 test('firs character with health = NaN', () => {
 	const list = [
-	  {name: 'мечник', health: 10},
-	  {name: 'маг', health: 'bad'},
-	  {name: 'лучник', health: 80},
+		{name: 'мечник', health: 10},
+		{name: 'маг', health: 'bad'},
+		{name: 'лучник', health: 80},
 	];
 	const received = sortCharacterByHealth(list);
 	const expected = [
-	  {name: 'лучник', health: 80},
-	  {name: 'мечник', health: 10},
-	  {name: 'маг', health: 'bad'},
+		{name: 'лучник', health: 80},
+		{name: 'мечник', health: 10},
+		{name: 'маг', health: 'bad'},
 	];
 
 	expect(received).toEqual(expected);
@@ -26,15 +26,15 @@ test('firs character with health = NaN', () => {
 
 test('unsorting characters', () => {
 	const list = [
-  	{name: 'мечник', health: 10},
-	  {name: 'маг', health: 100},
-  	{name: 'лучник', health: 80},
+		{name: 'мечник', health: 10},
+		{name: 'маг', health: 100},
+		{name: 'лучник', health: 80},
 	];
 	const received = sortCharacterByHealth(list);
 	const expected = [
-	  {name: 'маг', health: 100},
-  	{name: 'лучник', health: 80},
-  	{name: 'мечник', health: 10},
+		{name: 'маг', health: 100},
+		{name: 'лучник', health: 80},
+		{name: 'мечник', health: 10},
 	];
 
 	expect(received).toEqual(expected);
